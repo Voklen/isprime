1a9f50a2b2559f13fdea8f0a814dc69f31de2ae9
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-08-28 15:47:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# isprime
+
+A (very serious) API to check if a number is prime.
+
+Run with:
+```
+cargo run
+```
+
+And you may now access the api on http://127.0.0.1:8080/api/isprime/6
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
+
+#[get("/api/isprime/{number}")]
+async fn greet(num_string: web::Path<String>) -> impl Responder {
+    let any_num: i32 = match num_string.parse() {
+        Ok(any_num) => any_num,
+        Err(_) => return r#"{"error": "This operation isn't prime for success"}"#.to_owned(),
+    };
+    let num = match u32::try_from(any_num) {
+        Ok(num) => num,
+        Err(_) => {
+            return r#"{"error": "Negative number. Please upgrade to isPrime API Premium or Enterprise for support"}"#.to_owned()
+        }
+    };
+    let is_prime = is_prime(num);
+    format!("{{\"isprime\": {is_prime}}}")
+}
+
+/// Test whether a number is prime. Checks every odd number up to `sqrt(n)`.
+pub fn is_prime(n: u32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    firstfac(n) == n
+}
+
+/// Find the first factor (other than 1) of a number
+fn firstfac(x: u32) -> u32 {
+    if x % 2 == 0 {
+        return 2;
+    };
+    // TODO: return to step_by
+    // for n in (3..).step_by(2).take_while(|m| m*m <= x) {
+    for n in (1..).map(|m| 2 * m + 1).take_while(|m| m * m <= x) {
+        if x % n == 0 {
+            return n;
+        };
+    }
+    // No factor found. It must be prime.
+    x
+}
